c6b4528035cdc0e453ff074afc35ee3317ae325d
Oliver Smith <blugammadesign@gmail.com>
2018-11-01 16:15:32 +0000

    Setup node.js express server code

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-//Use the library MQTT (this exists in the node_modules folder)
-var mqtt = require('mqtt')
-var MQTT_TOPIC = "iFrame"; //MQTT topis is test
+
+const express = require('express');
+const bodyParser = require(`body-parser`);
+const app = express();
+const port = 2323;
+
+var mqtt = require('mqtt');
+var MQTT_TOPIC = "iFrame";
 var MQTT_ADDR = "mqtt://broker.i-dat.org:80"; //Use the broker address here
 var MQTT_PORT = 80; //And broker's port
 
 var client  = mqtt.connect(MQTT_ADDR); //Create a new connection (use the MQTT adress)
-var numToGo = 0; //Initialise a number variable to 0
+
 
 //MQTT
 client.on('connect', function() { //connect the MQTT client
     client.subscribe(MQTT_TOPIC, { qos: 1 }); //Subscribe to the topic
 
    
-      numToGo = getRandom(100); //Create a random number from 0 to 100
+      
 
       //Create a new variable that will store information as text (String)
       //Concatenate the name with the random number (as string)
-      var message = "Name: "+numToGo.toString();
+      var message = "Name";
       client.publish(MQTT_TOPIC, message); //Publish the message of the client
       console.log(message); //Print the results on the console (i.e. Terminal)
 });
 
-//Function that helps us to create a random number
-function getRandom(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
+app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
-client.on('error', function(){
-    console.log("ERROR")
-    client.end()
-})
-client.on('offline', function() {
-    console.log("offline");
+app.listen(port, () => {
+    console.log(`App is listening to ${port}`);
 });
 
-client.on('reconnect', function() {
-    console.log("reconnect");
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/www/index.html');
 });
-*/
